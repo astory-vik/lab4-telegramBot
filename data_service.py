a28418c866aa1916061efae63f05084e61ced08e
astory-vik <A.Astafyeva.FIT.122.20@knute.edu.ua>
2020-10-13 13:22:08 +0300

    Array

--- data_service.py
+++ data_service.py
@@ -1,4 +1,17 @@
+import random
+
 f = open('data.txt', 'r', encoding="UTF-8")
 arr = []
 for eachLine in f:
     arr.append(eachLine)
+
+
+def RandomHoroscope():
+    try:
+        b = int(random.randint(0, len(arr) - 1))
+        str = arr[b]
+        arr.pop(b)
+    except ValueError:
+        print('null')
+    return str
+
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import telebot
+from telebot import types
 import random
-from data_service import arr
+from data_service import RandomHoroscope
+from telebot import types
 bot = telebot.TeleBot('1206876123:AAHyHKoSVbxfcHFOf7XzYlGUYwLmoFLUbDA')
+markup = types.ReplyKeyboardMarkup()
+markup.row('Выдать гороскоп', 'help')
+
 
 
 
@@ -9,9 +14,22 @@ bot = telebot.TeleBot('1206876123:AAHyHKoSVbxfcHFOf7XzYlGUYwLmoFLUbDA')
 def command_start(message):
     bot.send_message(message.chat.id, "Привет, я гороскоп бот")
 
-@bot.message_handler(commands=['try'])
-def command_start(message):
-    rand = random.randint(0, len(arr)-1)
-    bot.send_message(message.chat.id, str(arr[rand]))
+
+@bot.message_handler(content_types=['text'])
+def send_horoscope(message):
+    if message.text == 'Выдать гороскоп':
+        bot.send_message(message.chat.id, RandomHoroscope())
+    elif message.text.lower() == 'help':
+        bot.send_message(message.chat.id, "Привет я гороскоп БОТ, я предсказываю будущее и не только)")
+
+
+# @bot.message_handler(commands=['try'])
+# def command_start(message):
+#   rand = random.randint(0, len(arr)-1)
+#   bot.send_message(message.chat.id, str(arr[rand]))
+
 if __name__ == '__main__':
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('Выдать гороскоп', 'help')
     bot.polling(none_stop=True)
+
